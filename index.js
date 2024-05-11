c4601985d748f5b0ee7265baeccdc8b5ee1d8eb6
HamimBhai742 <omagootruelove147@gmail.com>
2024-05-11 15:08:17 +0600

    cors update

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const app = express()
 const port = process.env.PORT || 5000
 
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: [
+            "http://localhost:5173",
+            "https://alternative-products.web.app",
+            "https://alternative-products.firebaseapp.com",
+        ],
+        credentials: true,
+    }
+))
 app.use(express.json())
 
 

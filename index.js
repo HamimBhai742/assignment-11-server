033ca5df45b5c4215bb148e7a54f5a84292662b3
HamimBhai742 <omagootruelove147@gmail.com>
2024-05-11 13:16:05 +0600

    my aueries api create

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bls3tyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +28,29 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
+        const database = client.db("queriesDB");
+        const queriesCollection = database.collection("queries");
+
+        app.post('/add-queries', async (req, res) => {
+            const addQueries = req.body
+            // console.log(addQueries);
+            const result = await queriesCollection.insertOne(addQueries);
+            res.send(result)
+        })
+
+        app.get('/my-queries', async (req, res) => {
+            const cursor = queriesCollection.find()
+            const result = await cursor.sort({ _id: -1 }).toArray()
+            res.send(result)
+        })
+      
+        app.get('/my-queries/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await queriesCollection.findOne(query);
+            res.send(result)
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
